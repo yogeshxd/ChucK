885ed46d1a1c8d931ca8d75fa407f561145facd2
Yogesh <45172882+yogeshxd@users.noreply.github.com>
2022-12-26 17:21:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ author = 626811868249325578 #change this to your discord id
 token = "" #put your bot token in here
 ext = "Du " #put your bot prefix here
 #put you rapidapi key in cogs/chatai.py (goto https://rapidapi.com/waifuai/api/waifu/)
-wh = ""
+wh = "" #Your discord webhook url for log messages
